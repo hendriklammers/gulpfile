483b74ef169b19d3bc2694296acd4c8934e9224c
Hendrik Lammers <lammers@gmail.com>
2014-07-27 14:50:52 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function foo(bla) {
+    console.log(bla);
+}
+
+foo();
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    prefix = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    concat = require('gulp-concat');
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './build'
+        },
+        notify: true
+    });
+});
+
+gulp.task('scripts', function() {
+    gulp.src('app/scripts/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish))
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('html', function() {
+    gulp.src('app/index.html')
+        .pipe(gulp.dest('./build'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('styles', function() {
+    gulp.src('app/sass/styles.scss')
+        .pipe(sass({sourcemap: true, sourcemapPath: './app/sass', style: 'compact'}))
+        .pipe(prefix('last 2 versions', '> 1%', 'ie 9', 'ie 8'))
+        .pipe(gulp.dest('build/css'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('default', ['styles', 'html', 'scripts']);
+
+gulp.task('watch', ['styles', 'html', 'scripts', 'browser-sync'], function () {
+    gulp.watch('app/sass/*.scss', ['styles']);
+    gulp.watch('app/index.html', ['html']);
+});
