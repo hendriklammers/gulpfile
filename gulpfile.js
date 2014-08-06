c4de5a9eecaed1a87113c87e44e89c25265ed9ab
Hendrik Lammers <lammers@gmail.com>
2014-08-06 22:30:35 +0200

    Change folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -6,46 +6,43 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     jshint = require('gulp-jshint'),
     stylish = require('jshint-stylish'),
-    concat = require('gulp-concat'),
-    filter = require('gulp-filter');
+    concat = require('gulp-concat');
 
 gulp.task('browser-sync', function() {
     browserSync.init({
+        files: [
+            'index.html',
+            'assets/*'
+        ],
         server: {
-            baseDir: ['build', 'app']
+            baseDir: ['./']
         },
         notify: true
     });
 });
 
-gulp.task('scripts', function() {
-    return gulp.src('app/scripts/**/*.js')
+gulp.task('jshint', function() {
+    return gulp.src('src/app.js')
         .pipe(jshint())
-        .pipe(jshint.reporter(stylish))
-        .pipe(concat('bundle.js'))
-        .pipe(gulp.dest('./build/js'))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(jshint.reporter(stylish));
 });
 
-gulp.task('html', function() {
-    return gulp.src('app/index.html')
-        .pipe(gulp.dest('./build'))
-        .pipe(browserSync.reload({stream: true}));
+gulp.task('scripts', ['jshint'], function() {
+    return gulp.src(['src/app.js'])
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('assets/js'));
 });
 
 gulp.task('styles', function() {
-    return gulp.src('app/sass/**/*.scss')
+    return gulp.src('sass/**/*.scss')
         .pipe(sass({style: 'compact', sourcemap: true, sourcemapPath: '../sass'}))
         .pipe(prefix('last 2 versions', '> 1%', 'ie 9', 'ie 8'))
-        .pipe(gulp.dest('build/css'))
-        .pipe(filter('**/*.css'))   // Filter stream to only css files
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(gulp.dest('assets/css'));
 });
 
-gulp.task('default', ['clean', 'styles', 'html', 'scripts']);
+gulp.task('default', ['styles', 'scripts']);
 
-gulp.task('watch', ['styles', 'html', 'scripts', 'browser-sync'], function () {
-    gulp.watch('app/sass/*.scss', ['styles']);
-    gulp.watch('app/index.html', ['html']);
-    gulp.watch('app/scripts/**/*.js', ['scripts']);
+gulp.task('watch', ['styles', 'scripts', 'browser-sync'], function () {
+    gulp.watch('sass/*.scss', ['styles']);
+    gulp.watch('src/**/*.js', ['scripts']);
 });
